452fc20bc72cc71c0eead8b20334fb230836d96e
DuyTH <Duythse130601@fpt.edu.vn>
2020-07-27 21:03:39 +0700

    fix bug

--- login_facebook/lib/network_provider/authentication_network_provider.dart
+++ login_facebook/lib/network_provider/authentication_network_provider.dart
@@ -11,8 +11,6 @@ import 'package:loginfacebook/setting/setting.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-
-
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 final _facebooklogin = FacebookLogin();
@@ -20,148 +18,140 @@ String name;
 String email;
 String imageUrl;
 String phoneNumber;
-UserAuthenticated currentUserWithToken ;
+UserAuthenticated currentUserWithToken;
 final FirebaseMessaging _fcm = FirebaseMessaging();
 
+class AccountNetworkProvider {
+  String baseUrl = Setting.baseUrl + 'Account/';
 
-class AccountNetworkProvider{
-  String baseUrl =Setting.baseUrl+'Account/';
-  
   Future<UserAuthenticated> fetchUser(String IdToken, String fcmToken) async {
-  String authenticateUrl = baseUrl+ 'authenticate';
-  final user = await _auth.currentUser();
+    String authenticateUrl = baseUrl + 'authenticate';
+    final user = await _auth.currentUser();
 
-
-  String jsonString = '{'
-  +'"idToken": "'+IdToken+'",'
-  +'"fcmToken": "'+fcmToken+'"'
-+'}';
-  final http.Response response = await http.post(authenticateUrl,headers:  {
-        HttpHeaders.contentTypeHeader: 'application/json'
-      }, body: jsonString  );
-  if (response.statusCode == 200) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    UserAuthenticated rs =  UserAuthenticated.fromJson(json.decode(response.body));
-    currentUserWithToken =rs;
-    return rs;
-  } else {
-    throw Exception('Failed to load user');
+    String jsonString = '{' +
+        '"idToken": "' +
+        IdToken +
+        '",' +
+        '"fcmToken": "' +
+        fcmToken +
+        '"' +
+        '}';
+    final http.Response response = await http.post(authenticateUrl,
+        headers: {HttpHeaders.contentTypeHeader: 'application/json'},
+        body: jsonString);
+    if (response.statusCode == 200) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON.
+      UserAuthenticated rs =
+          UserAuthenticated.fromJson(json.decode(response.body));
+      currentUserWithToken = rs;
+      return rs;
+    } else {
+      throw Exception('Failed to load user');
+    }
   }
-}
 
- 
   Future<UserAuthenticated> fetchUserByJWT() async {
     final prefs = await SharedPreferences.getInstance();
     String token = prefs.getString("JwtToken");
-  String authenticateUrl = baseUrl+ 'authenticate';
-  final http.Response response = await http.get(authenticateUrl,headers:  {
-        HttpHeaders.contentTypeHeader: 'application/json',
-        HttpHeaders.authorizationHeader: 'Bearer ' + token
-      });
-  if (response.statusCode == 200) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    UserAuthenticated rs =  UserAuthenticated.fromJson(json.decode(response.body));
-    currentUserWithToken =rs;
-    prefs.setString("JwtToken", currentUserWithToken.Token);
-    return rs;
-  } else {
-    throw Exception('Failed to load user');
+    String authenticateUrl = baseUrl + 'authenticate';
+    final http.Response response = await http.get(authenticateUrl, headers: {
+      HttpHeaders.contentTypeHeader: 'application/json',
+      HttpHeaders.authorizationHeader: 'Bearer ' + token
+    });
+    if (response.statusCode == 200) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON.
+      UserAuthenticated rs =
+          UserAuthenticated.fromJson(json.decode(response.body));
+      currentUserWithToken = rs;
+      prefs.setString("JwtToken", currentUserWithToken.Token);
+      return rs;
+    } else {
+      throw Exception('Failed to load user');
+    }
   }
 }
-}
 
-class FirebaseNetworkProvider{
-  AccountNetworkProvider accountNetworkProvider =new  AccountNetworkProvider();  
+class FirebaseNetworkProvider {
+  AccountNetworkProvider accountNetworkProvider = new AccountNetworkProvider();
 
-Future<UserAuthenticated> signInWithGoogle() async {
-  final prefs = await SharedPreferences.getInstance();
-  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
+  Future<UserAuthenticated> signInWithGoogle() async {
+    final prefs = await SharedPreferences.getInstance();
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
 
-  final AuthCredential credential = GoogleAuthProvider.getCredential(
-    accessToken: googleSignInAuthentication.accessToken,
-    idToken: googleSignInAuthentication.idToken,
-  );
-  final AuthResult authResult = await _auth.signInWithCredential(credential);
-  final FirebaseUser user = authResult.user;
-  assert(user.email != null);
-  if (user.email == null) {
-    return null;
-  } else {
-    name = user.displayName;
-    phoneNumber = user.phoneNumber;
-    email = user.email;
-    imageUrl = user.photoUrl;
-    final tokenId =await user.getIdToken();
-    final token = tokenId.token ; 
-    currentUserWithToken = await accountNetworkProvider.fetchUser(tokenId.token, await _fcm.getToken());
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+    final AuthResult authResult = await _auth.signInWithCredential(credential);
+    final FirebaseUser user = authResult.user;
+    final tokenId = await user.getIdToken();
+    final token = tokenId.token;
+    currentUserWithToken = await accountNetworkProvider.fetchUser(
+        tokenId.token, await _fcm.getToken());
     prefs.setString("JwtToken", currentUserWithToken.Token);
-    if (name.contains(" ")) {
-      name = name.substring(0, name.indexOf(" "));
-    }
-    final idtoken =await user.getIdToken();      
+    final idtoken = await user.getIdToken();
     assert(!user.isAnonymous);
     assert(await user.getIdToken() != null);
     final FirebaseUser currentUser = await _auth.currentUser();
     assert(user.uid == currentUser.uid);
     return currentUserWithToken;
   }
-}
 
-Future<UserAuthenticated> loginWithFacebook() async {
-try{
-
-  // Gọi hàm LogIn() với giá trị truyền vào là một mảng permission
-  // Ở đây mình truyền vào cho nó quền xem email
-  _facebooklogin.loginBehavior = FacebookLoginBehavior.webViewOnly;
+  Future<UserAuthenticated> loginWithFacebook() async {
+    try {
+      // Gọi hàm LogIn() với giá trị truyền vào là một mảng permission
+      // Ở đây mình truyền vào cho nó quền xem email
+      _facebooklogin.loginBehavior = FacebookLoginBehavior.webViewOnly;
 //    _facebooklogin.loginBehavior = FacebookLoginBehavior.nativeOnly;
-  final result = await _facebooklogin.logIn(['email']);
-  // Kiểm tra nếu login thành công thì thực hiện login Firebase
-  // (theo mình thì cách này đơn giản hơn là dùng đường dẫn
-  // hơn nữa cũng đồng bộ với hệ sinh thái Firebase, tích hợp được
-  // nhiều loại Auth
+      final result = await _facebooklogin.logIn(['email']);
+      // Kiểm tra nếu login thành công thì thực hiện login Firebase
+      // (theo mình thì cách này đơn giản hơn là dùng đường dẫn
+      // hơn nữa cũng đồng bộ với hệ sinh thái Firebase, tích hợp được
+      // nhiều loại Auth
 
-  if (result.status == FacebookLoginStatus.loggedIn) {
-    final credential = FacebookAuthProvider.getCredential(
-      accessToken: result.accessToken.token,
-    );
-    // Lấy thông tin User qua credential có giá trị token đã đăng nhập
-    final user = (await _auth.signInWithCredential(credential)).user;
-   
-      name = user.displayName;
-      phoneNumber = user.phoneNumber;
-      //email = user.email;
-      imageUrl = user.photoUrl;
-      final tokenId =await user.getIdToken();
-      currentUserWithToken = await accountNetworkProvider.fetchUser(tokenId.token, await _fcm.getToken());
-      if (name.contains(" ")) {
-        name = name.substring(0, name.indexOf(" "));
+      if (result.status == FacebookLoginStatus.loggedIn) {
+        final credential = FacebookAuthProvider.getCredential(
+          accessToken: result.accessToken.token,
+        );
+        // Lấy thông tin User qua credential có giá trị token đã đăng nhập
+        final user = (await _auth.signInWithCredential(credential)).user;
+
+        name = user.displayName;
+        phoneNumber = user.phoneNumber;
+        //email = user.email;
+        imageUrl = user.photoUrl;
+        final tokenId = await user.getIdToken();
+        currentUserWithToken = await accountNetworkProvider.fetchUser(
+            tokenId.token, await _fcm.getToken());
+        if (name.contains(" ")) {
+          name = name.substring(0, name.indexOf(" "));
+        }
+        return currentUserWithToken;
       }
-    return currentUserWithToken;
+      print("object");
+    } catch (e) {
+      print(e);
+    }
   }
-  print("object");
-}catch(e){
-print(e);
-}
-}
+
   Future logout() async {
     await _auth.signOut();
     await _facebooklogin.logOut();
     await googleSignIn.signOut();
-    checkedInStore=null;
-    currentUserWithToken=null;
+    checkedInStore = null;
+    currentUserWithToken = null;
   }
 
-Future<bool> checkLogin() async {
-    final user = await _auth.currentUser();
-    if (user != null) {
-      currentUserWithToken = await accountNetworkProvider.fetchUserByJWT();
-      return true;
-    }else 
+  Future<bool> checkLogin() async {
+    // final user = await _auth.currentUser();
+    // if (user != null) {
+    //   currentUserWithToken = await accountNetworkProvider.fetchUserByJWT();
+    //   return true;
+    // }else
     return false;
   }
 }
-
